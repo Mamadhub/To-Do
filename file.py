45277f7bd6b02b806cd8f1bc7a596d3e4a723938
Mamadhub <141618496+Mamadhub@users.noreply.github.com>
2023-09-04 22:32:32 +0330

    create class for change files

---
+++ file.py
@@ -0,0 +1,13 @@
+class file:
+    def files_write(name):
+        files = open("filess/files.txt", 'a')
+
+    def files_read():
+        files = open("filess/files.txt", 'r')
+            
+    def load_file(number):
+        pass
+    
+f=file()
+
+f.files_write()
