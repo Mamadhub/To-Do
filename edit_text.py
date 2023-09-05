89584921e6a66c1116bb7844c2cfca2d4b7080c5
Mamadgh <141618496+Mamadhub@users.noreply.github.com>
2023-09-05 23:12:35 +0330

    add error test

--- edit_text.py
+++ edit_text.py
@@ -4,17 +4,45 @@ from file_s import filesss
 f = filesss()
 
 def add_text():
-    name = input('please enter name file :')
-    text = input("text :")
-    f.add_text(name, text)
-    f.files_data[name] += text
+    
+    while 1:
+        try:
+            name = input('please enter name file :')
+            text = input("text :")
+            f.add_text(name, text)
+            f.files_data[name] += text
+            break
+        except:
+            again = input("not found. again(y/n):")
+            if again.lower() == 'n':
+                break
+            else:
+                continue
 
 def delete_file():
-    name = input('please enter name file :')
-    os.remove(f'filess/{name}.txt')
+    while 1:
+        try:
+            name = input('please enter name file :')
+            os.remove(f'filess/{name}.txt')
+            break
+        except:
+            again = input("not found. again(y/n):")
+            if again.lower() == 'n':
+                break
+            else:
+                continue
+    
     
 def edit_name():
-    name = input('please enter name file :')
-    new_name = input('please enter name file :')
-    os.rename(f'filess/{name}.txt',f'filess/{new_name}.txt' )
-    
+    while 1:
+        try:
+            name = input('please enter name file :')
+            new_name = input('please enter new name file :')
+            os.rename(f'filess/{name}.txt',f'filess/{new_name}.txt' )
+            break
+        except:
+            again = input("not found. again(y/n):")
+            if again.lower() == 'n':
+                break
+            else:
+                continue
